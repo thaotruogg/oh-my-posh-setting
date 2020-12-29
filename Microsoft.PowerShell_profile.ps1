2495000e98ea6250ec5717256784401ad7cf1125
Thao Truong <43067475+thaotruogg@users.noreply.github.com>
2020-12-30 00:52:05 +0700

    init commit
    Signed-off-by: Thao Truong <43067475+thaotruogg@users.noreply.github.com>

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,39 @@
+[ScriptBlock]$Prompt = {
+    $lastCommandSuccess = $?
+    $realLASTEXITCODE = $global:LASTEXITCODE
+    $errorCode = 0
+    if ($lastCommandSuccess -eq $false) {
+        #native app exit code
+        if ($realLASTEXITCODE -is [int]) {
+            $errorCode = $realLASTEXITCODE
+        }
+        else {
+            $errorCode = 1
+        }
+    }
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = "C:\tools\oh-my-posh.exe"
+    $cleanPWD = $PWD.ProviderPath.TrimEnd("\")
+    $startInfo.Arguments = "-config=""$env:USERPROFILE\.poshthemes\agnoster.omp.json"" -error=$errorCode -pwd=""$cleanPWD"""
+    $startInfo.Environment["TERM"] = "xterm-256color"
+    $startInfo.CreateNoWindow = $true
+    $startInfo.StandardOutputEncoding = [System.Text.Encoding]::UTF8
+    $startInfo.RedirectStandardOutput = $true
+    $startInfo.UseShellExecute = $false
+    if ($PWD.Provider.Name -eq 'FileSystem') {
+      $startInfo.WorkingDirectory = $PWD.ProviderPath
+    }
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $startInfo
+    $process.Start() | Out-Null
+    $standardOut = $process.StandardOutput.ReadToEnd()
+    $process.WaitForExit()
+    $standardOut
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    #remove temp variables
+    Remove-Variable realLASTEXITCODE -Confirm:$false
+    Remove-Variable lastCommandSuccess -Confirm:$false
+}
+Set-Item -Path Function:prompt -Value $Prompt -Force
+
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
